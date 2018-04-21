ee5d9572b045f26499a894d9fba1491c497c4b21
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-21 17:59:33 -0400

    complete

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -8,22 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISlider *amountOfPeopleSlider;
+
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.amountTextField.delegate = self;
+    [self.amountTextField addTarget:self
+                                 action:@selector(textFieldDidChange)
+                       forControlEvents:UIControlEventEditingChanged];
+    [self.amountOfPeopleSlider addTarget:self
+                    action:@selector(textFieldDidChange)
+          forControlEvents:UIControlEventValueChanged];
+    
+    [self.tipSlider addTarget:self
+                                  action:@selector(textFieldDidChange)
+                        forControlEvents:UIControlEventValueChanged];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)textFieldDidChange
+{
+    [self calculateSplitAmount];
 }
 
+- (CGFloat)calculateTip
+{
+    float tipToCalculate = [self.amountTextField.text floatValue];
+    
+    float tipPercent = (int)self.tipSlider.value;
+    NSNumberFormatter *fmt = [[NSNumberFormatter alloc]init];
+    self.tipLabel.text = [NSString stringWithFormat:@"%d %@" ,(int)tipPercent,[fmt percentSymbol]];
+    
+    tipPercent = tipPercent / 100;
+    
+    return (tipToCalculate * tipPercent);
+    
+}
+
+- (void)calculateSplitAmount
+{
+    float totalAmount = [self.amountTextField.text floatValue] + [self calculateTip];
+    float splitAmount = totalAmount / (int)self.amountOfPeopleSlider.value;
+    self.resultLabel.text = [NSString stringWithFormat:@"%0.2f",splitAmount];
+}
+
+- (IBAction)adjustSlider:(UISlider *)sender
+{
+    self.sliderLabel.text = [NSString stringWithFormat:@"%d",(int)sender.value];
+}
 
 @end
